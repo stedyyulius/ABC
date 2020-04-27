f8d6d6ecfc0187055b60fca3d5418313e893a0e2
stedyyulius <stedyyulius@gmail.com>
2020-04-27 16:29:56 +0700

    hide

--- start.js
+++ start.js
@@ -1,8 +1,8 @@
 var config = {
-    apiKey: "AIzaSyA0PzkgtUm7TUvIweYfkoEFD_1YnF_daHw",
-    authDomain: "tracker-b765b.firebaseapp.com",
-    databaseURL: "https://tracker-b765b.firebaseio.com/",
-    storageBucket: "bucket.appspot.com",
+    apiKey: process.env.key,
+    authDomain: process.env.domain,
+    databaseURL: process.env.url,
+    storageBucket: process.env.bucket,
 };
 firebase.initializeApp(config);
 // console.log(firebase);
@@ -10,54 +10,54 @@ firebase.initializeApp(config);
 // // Get a reference to the database service
 var database = firebase.database();
 
-// function getLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition);
-//     } else {
-//         console.log("Geolocation is not supported by this browser.")
-//     }
-// }
+function getLocation() {
 
-// function showPosition(position) {
-//     fetch("https://ipapi.co/json/")
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (ip_details) {
-//             const data = {
-//                 lat: position.coords.latitude,
-//                 longitude: position.coords.latitude,
-//                 time: new Date(position.timestamp).toString(),
-//                 ...ip_details,
-//             };
-//             console.log(ip_details);
-//             firebase
-//                 .database()
-//                 .ref(
-//                     "visitor/" + ip_details.city + "-" + new Date().getTime() + "/",
-//                 )
-//                 .set(data);
-//             console.log(data);
-//         });
-// }
+    fetch("https://ipapi.co/json/")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (ip_details) {
+            const data = {
+                time: new Date().toString(),
+                ...ip_details,
+            };
+            // console.log(ip_details);
+            firebase
+                .database()
+                .ref(
+                    "visitor/" + ip_details.city + "-" + new Date().getTime() + "/",
+                )
+                .set(data);
+            // console.log(data);
+        });
 
-// getLocation();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
 
-fetch("https://ipapi.co/json/")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (ip_details) {
-        const data = {
-            time: new Date().toString(),
-            ...ip_details,
-        };
-        // console.log(ip_details);
-        firebase
-            .database()
-            .ref(
-                "visitor/" + ip_details.city + "-" + new Date().getTime() + "/",
-            )
-            .set(data);
-        // console.log(data);
-    });
+function showPosition(position) {
+    fetch("https://ipapi.co/json/")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (ip_details) {
+            const data = {
+                lat: position.coords.latitude,
+                lng: position.coords.latitude,
+                time: new Date(position.timestamp).toString(),
+                ...ip_details,
+            };
+            console.log(ip_details);
+            firebase
+                .database()
+                .ref(
+                    "visitor/" + ip_details.city + "-" + new Date().getTime() + "/",
+                )
+                .set(data);
+            console.log(data);
+        });
+}
+
+getLocation();
+
